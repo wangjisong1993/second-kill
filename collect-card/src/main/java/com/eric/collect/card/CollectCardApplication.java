266d465b98dbbaaa8015d0b61ff98c6276315b7a
忘记送 <2652644731@qq.com>
2019-01-28 19:02:14 +0800

    1. 支付宝集福

---
+++ collect-card/src/main/java/com/eric/collect/card/CollectCardApplication.java
@@ -0,0 +1,27 @@
+package com.eric.collect.card;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 模拟支付宝集福活动
+ *
+ * @author wang.js on 2019/1/28.
+ * @version 1.0
+ */
+@SpringBootApplication
+@MapperScan("com.eric.collect.card.dao")
+@ComponentScan({"com.eric"})
+@EnableFeignClients
+@EnableDiscoveryClient
+public class CollectCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectCardApplication.class, args);
+	}
+
+}
